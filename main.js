dc581f0e7922368fcef196eb1a903f964f95f12b
Julian <96155980+wulfiss@users.noreply.github.com>
2022-11-07 19:27:52 -0300

    prevent from modify a cell already taken and more

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 (function ticTacToe(){
 
+    const players ={
+        
+           playerOne: {name: '',
+            marker: 'X',
+            point: 00},       
+              
+            playerTwo: {name: '',
+            marker: 'O',
+            pointer: 00},
+        
+            turn: {turn:'playerOne'},
+    }
+    
     const input = () => {
         let playerX = document.querySelector('#pONE');
         let playerO = document.querySelector('#pTWO');
@@ -13,16 +26,38 @@
             $playAgain, $newGame, $gameBoardDiv
         }
     }
+    
+
+    function playerNames(){
+        players['playerOne']['name'] = input()['playerX'].value;
+        players['playerTwo']['name'] = input()['playerO'].value;
+    }
 
     const boardDisplay = () =>{
         input()['$gameBoardDiv'].addEventListener('click', (e) => {
+            if(e.target.dataset.turn == 'free'){
+                e.target.textContent = 'X';
+                e.target.dataset.turn = 'used';
+            }else if(e.target.dataset.turn == 'used'){
+                return false;
+            }/*
             console.log(e.target.getAttribute('data-key'));
+            console.log(e.target.dataset.turn);
+            e.target.dataset.turn = false;
+            console.log(e.target.dataset.turn);
+            */
         })
     }
 
+    
     const letsPlay = () => {
         input()['$letsPlay'].addEventListener('click', (e) => {
             input();
+            playerNames();
+            console.log(players.playerOne.name);
+            console.log(players.playerTwo.name);
+            console.log(players.turn);
+            
             boardDisplay();
         })
     }
